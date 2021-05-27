c4bc4d43733c5d5bd809b5a163b99f18a8a13255
Michael Todd Maxey <41805243+ToddMaxey@users.noreply.github.com>
2021-05-27 18:56:05 -0500

    Add files via upload

--- ProxySettings.ps1
+++ ProxySettings.ps1
@@ -20,46 +20,107 @@ $RegPathHKU = "HKU:\S-1-5-18\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet 
 $RegPathHKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings"
 $RegPathDefault = "HKU:\.DEFAULT\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings"
 
-Write-Host "WinHTTP Proxy setting for current run context"(whoami)(netsh winhttp show proxy)
+Write-Host
+Write-Host "Proxy settings for user"(whoami)"for machine"(hostname) -ForegroundColor Yellow -BackgroundColor Blue
+Write-Host
+
+#(netsh winhttp show proxy)
+
+    if (Get-RegistryValue -Path $RegPathHKLM\Connections -Value "WinHttpSettings") {
+        Write-Host
+		Write-Host "WinHttpSettings proxy settings via NETSH on the Local Machine were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+        Write-Host "netsh winhttp show proxy Results" (netsh winhttp show proxy) -ForegroundColor Yellow
+        Write-Host
+		Write-Host "The WinHttpSettings in machine path (HKLM) are :  "  (Get-RegistryValue -Path $RegPathHKLM\Connections -Value "WinHttpSettings" ) -ForegroundColor Yellow
+        Write-Host
+        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathHKLM\Connections -Value "WinHttpSettings" | Format-Hex) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "WinHttpSettings proxy settings on the Local Machine were detected" -ForegroundColor Green
+            Write-Host}
 
     if (Get-RegistryValue -Path $RegPathHKLM -Value "ProxyServer") {
-		Write-Host "Proxy settings on the Local Machine were detected"
-		Write-Host "The detected Proxy settings in machine path (HKLM) are :  "  (Get-RegistryValue -Path $RegPathHKLM -Value "ProxyServer" )
-        Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathHKLM -Value "ProxyEnable" ) 
-	}
-    if (Get-RegistryValue -Path $RegPathHKLM\Connections -Value "WinHttpSettings") {
-		Write-Host "WinHttpSettings on the Local Machine were detected"
-		Write-Host "The WinHttpSettings in machine path (HKLM) are :  "  (Get-RegistryValue -Path $RegPathHKLM\Connections -Value "WinHttpSettings" )
-        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathHKLM\Connections -Value "WinHttpSettings" | Format-Hex)
-	} 
+        Write-Host
+		Write-Host "Proxy settings on the Local Machine were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+		Write-Host "The detected Proxy settings in machine path (HKLM) are :  "  (Get-RegistryValue -Path $RegPathHKLM -Value "ProxyServer" ) -ForegroundColor Yellow
+        Write-Host
+        Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathHKLM -Value "ProxyEnable" ) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "Proxy settings on the Local Machine were NOT detected" -ForegroundColor Green
+            Write-Host}
+             
 	if (Get-RegistryValue -Path $RegPathHKU -Value "ProxyServer") {
-	    Write-Host "Proxy settings in SYSTEM SID level were detected" 
-	    Write-Host "The detected proxy settings in SYSTEM HKU path (S-1-5-18) are :  "  (Get-RegistryValue -Path $RegPathHKU -Value "ProxyServer" ) 
-		Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathHKU -Value "ProxyEnable" ) 
-	}
+        Write-Host
+	    Write-Host "Proxy settings in SYSTEM SID level were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+	    Write-Host "The detected proxy settings in SYSTEM HKU path (S-1-5-18) are :  "  (Get-RegistryValue -Path $RegPathHKU -Value "ProxyServer" ) -ForegroundColor Yellow
+        Write-Host
+		Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathHKU -Value "ProxyEnable" ) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "Proxy settings in SYSTEM SID level were NOT detected" -ForegroundColor Green
+            Write-Host}
+
     if (Get-RegistryValue -Path $RegPathHKU\Connections -Value "WinHttpSettings") {
-		Write-Host "WinHttpSettings on the Local Machine were detected"
-		Write-Host "The WinHttpSettings in SYSTEM HKU path (S-1-5-18) are :  "  (Get-RegistryValue -Path $RegPathHKU\Connections -Value "WinHttpSettings" )
-        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathHKU\Connections -Value "WinHttpSettings" | Format-Hex) 
-	}  
+        Write-Host
+		Write-Host "WinHttpSettings in SYSTEM SID level were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+		Write-Host "The WinHttpSettings in SYSTEM HKU path (S-1-5-18) are :  "  (Get-RegistryValue -Path $RegPathHKU\Connections -Value "WinHttpSettings" ) -ForegroundColor Yellow
+        Write-Host
+        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathHKU\Connections -Value "WinHttpSettings" | Format-Hex) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "WinHttpSettings in SYSTEM SID level were NOT detected" -ForegroundColor Green
+            Write-Host}
+              
 	if (Get-RegistryValue -Path $RegPathHKCU -Value "ProxyServer") {
-	    Write-Host "Proxy setting in current user level were detected"
-		Write-Host "The detected proxy settings in current user path (HKCU) are :  "  (Get-RegistryValue -Path $RegPathHKCU -Value "ProxyServer" )
-		Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathHKCU -Value "ProxyEnable" )
-	}
+        Write-Host
+	    Write-Host "Proxy setting in current user" (whoami) "level were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+		Write-Host "The detected proxy settings in current user path (HKCU) are :  "  (Get-RegistryValue -Path $RegPathHKCU -Value "ProxyServer" ) -ForegroundColor Yellow
+        Write-Host
+		Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathHKCU -Value "ProxyEnable" ) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "Proxy setting in current user" (whoami) "level were NOT detected" -ForegroundColor Green
+            Write-Host}
+
     if (Get-RegistryValue -Path $RegPathHKCU\Connections -Value "WinHttpSettings") {
-		Write-Host "WinHttpSettings on the Local Machine were detected"
-		Write-Host "The WinHttpSettings in current user path (HKCU) are :  "  (Get-RegistryValue -Path $RegPathHKCU\Connections -Value "WinHttpSettings" )
-        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathHKCU\Connections -Value "WinHttpSettings" | Format-Hex) 
-	} 
+        Write-Host
+		Write-Host "WinHttpSettings on the current user" (whoami) "were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+		Write-Host "The WinHttpSettings in current user path (HKCU) are :  "  (Get-RegistryValue -Path $RegPathHKCU\Connections -Value "WinHttpSettings" ) -ForegroundColor Yellow
+        Write-Host
+        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathHKCU\Connections -Value "WinHttpSettings" | Format-Hex) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "WinHttpSettings on the current user" (whoami) "were NOT detected" -ForegroundColor Green
+            Write-Host}
+
 	if (Get-RegistryValue -Path $RegPathDefault -Value "ProxyServer") {
-	    Write-Host "Proxy setting in DEFAULT user level were detected"
-		Write-Host "The detected proxy settings in the default user path (.DEFAULT) are :  "  (Get-RegistryValue -Path $RegPathDefault -Value "ProxyServer" )
-		Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathDefault -Value "ProxyEnable" )
-	}
+        Write-Host
+	    Write-Host "Proxy setting in .DEFAULT user level were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+		Write-Host "The detected proxy settings in the default user path (.DEFAULT) are :  "  (Get-RegistryValue -Path $RegPathDefault -Value "ProxyServer" ) -ForegroundColor Yellow
+        Write-Host
+		Write-Host "ProxyEnable is set to :  "  (Get-RegistryValue -Path $RegPathDefault -Value "ProxyEnable" ) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "Proxy setting in .DEFAULT user level were NOT detected" -ForegroundColor Green
+            Write-Host}
+
     if (Get-RegistryValue -Path $RegPathDefault\Connections -Value "WinHttpSettings") {
-		Write-Host "WinHttpSettings on the .Default were detected"
-		Write-Host "The WinHttpSettings in machine path (.Default) are :  "  (Get-RegistryValue -Path $RegPathDefault\Connections -Value "WinHttpSettings" )
-        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathDefault\Connections -Value "WinHttpSettings" | Format-Hex) 
-	} 
+        Write-Host
+		Write-Host "WinHttpSettings on the .DEFAULT were detected" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host
+		Write-Host "The WinHttpSettings in machine path (.Default) are :  "  (Get-RegistryValue -Path $RegPathDefault\Connections -Value "WinHttpSettings" ) -ForegroundColor Yellow
+        Write-Host
+        Write-Host "WinHttpSettings is set to :  "  (Get-RegistryValue -Path $RegPathDefault\Connections -Value "WinHttpSettings" | Format-Hex) -ForegroundColor Yellow
+        Write-Host
+	} Else {Write-Host
+		    Write-Host "WinHttpSettings on the .DEFAULT were NOT detected" -ForegroundColor Green
+            Write-Host}
 
